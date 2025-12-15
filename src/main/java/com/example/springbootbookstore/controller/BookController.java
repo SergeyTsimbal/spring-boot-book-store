d2ed369caae00761a64025dee4d29aedd643bb6c
Serhii <sfisher@ukr.net>
2025-12-14 18:55:03 -0800

    Added CustomGlobalExceptionHandler and Validation

--- src/main/java/com/example/springbootbookstore/controller/BookController.java
+++ src/main/java/com/example/springbootbookstore/controller/BookController.java
@@ -4,6 +4,7 @@ import com.example.springbootbookstore.dto.BookDto;
 import com.example.springbootbookstore.dto.BookSearchParametersDto;
 import com.example.springbootbookstore.dto.CreateBookRequestDto;
 import com.example.springbootbookstore.service.BookService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -34,13 +35,13 @@ public class BookController {
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
 
     @PutMapping("/{id}")
     public BookDto updateBook(@PathVariable Long id,
-                              @RequestBody CreateBookRequestDto requestDto) {
+                              @RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.update(id, requestDto);
     }
 
--- src/main/java/com/example/springbootbookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/springbootbookstore/dto/CreateBookRequestDto.java
@@ -1,15 +1,27 @@
 package com.example.springbootbookstore.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateBookRequestDto {
+    @NotBlank
+    @Size(max = 255)
     private String title;
+    @NotBlank
+    @Size(max = 255)
     private String author;
+    @NotBlank
+    @Size(max = 255)
     private String isbn;
+    @NotNull
+    @Min(value = 0)
     private BigDecimal price;
     private String description;
     private String coverImage;
